9289126b3a60d9fa199b86fff9164ff18094ed09
Paulo Nascimento <victinhopaulo64@gmail.com>
2022-12-13 13:15:10 -0300

    primeiro

---
+++ src/pages/Inicial/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
+import firebase from "../../../src/firebaseConnection"
+import styles from "./style"
+import { FontAwesome } from "@expo/vector-icons"
+
+export default function Inicial(){
+    const [input, setInput] = useState(null)
+
+    function verificar
+    return (
+        <View>
+            <Text>Inicial Page</Text>
+        </View>
+    )
+}
